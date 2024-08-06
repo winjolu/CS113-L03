0bce40ffd3eb9c943aeac7e02c5b89ae64fd65ae
winjolu <winjolu@gmail.com>
2024-08-06 15:54:29 -0400

    commit

--- Term.java
+++ Term.java
@@ -1,165 +1,176 @@
-public class Term implements Comparable<Term> {           
-    //attributes, instance variables, data fields
-    private static final int DEFAULT_COEFFICIENT = 1; 
-    private static final int DEFAULT_EXPONENT = 1; 
+import java.util.LinkedList;
+import java.util.List;
+import java.util.ListIterator;
+import java.util.Collections;
+import java.util.Comparator;
 
-    private int coefficient;
-    private int exponent;
+/**
+ * Represents a polynomial composed of multiple terms.
+ */
+public class Polynomial {
 
-    //constructors
-    public Term() {
-        this.coefficient = DEFAULT_COEFFICIENT;
-        this.exponent = DEFAULT_EXPONENT;
+    private LinkedList<Term> polynomialList;
+
+    /**
+     * Default constructor initializing an empty polynomial.
+     */
+    public Polynomial() {
+        this.polynomialList = new LinkedList<>();
     }
 
-    public Term(int coefficient, int exponent) {
-        this.coefficient = coefficient;
-        this.exponent = exponent;
+    /** copy constructor */
+    public Polynomial(Polynomial otherPolyObj) {
+        if(otherPolyObj == null) return;
+        LinkedList<Term> poly = otherPolyObj.polynomialList;
+        this.polynomialList = new LinkedList<>(poly);
     }
 
-    public Term(String term) {
-        if (term.equals("")) {
-            this.coefficient = 0;
-            this.exponent = 0;
-        } else if (!term.contains("x")) {
-            this.coefficient = Integer.parseInt(term);
-            this.exponent = 0;
-        } else if (!term.contains("^") && term.contains("x")) {
-            if(term.equals("+x")) {
-                this.coefficient = 1;
-                this.exponent = 1;
-            } else if(term.equals("-x")) {
-                this.coefficient = -1;
-                this.exponent = 1;
-            } else {
-                String coefficientTerm = term.substring(0, term.length() - 1);
-                this.coefficient = Integer.parseInt(coefficientTerm);
-                this.exponent = 1;
+    
+    /**
+     * Adds a term to the polynomial. Combines terms with the same exponent.
+     * @param term The term to add to the polynomial.
+     */
+    public void addTerm(Term term) {
+        this.polynomialList.add(term);
+        // this.addLikeTerms();
+        // for (Term t : terms) {
+        //     if (t.getExponent() == term.getExponent()) {
+        //         t.setCoefficient(t.getCoefficient() + term.getCoefficient());
+        //         combined = true;
+        //         break;
+        //     }
+        // }
+        // if (!combined) {
+        //     terms.add(term);
+        // }
+    }
+    
+    //add like terms and sort
+    public void addLikeTerms() {
+       
+       LinkedList<Term> reducedPoly = new LinkedList<>();
+       while(!this.polynomialList.isEmpty()) {
+          int exponentTerm = this.polynomialList.get(0).getExponent(); 
+          int coefficientSum = this.polynomialList.get(0).getCoefficient();
+          this.polynomialList.removeFirst();
+          ListIterator<Term> iter = this.polynomialList.listIterator();
+          while(iter.hasNext()) {
+                Term term = iter.next();
+                if(term.getExponent() == exponentTerm) {
+                    coefficientSum += term.getCoefficient();
+                    iter.remove();
+                } 
+           }
+           if (coefficientSum != 0) {
+               reducedPoly.add(new Term(coefficientSum, exponentTerm));
+           }     
+       }
+       sortTerms(reducedPoly);
+       this.polynomialList = reducedPoly;   
+    }
+
+    //sort terms in descending order
+    public void sortTerms(LinkedList<Term> poly) {
+        Collections.sort(poly, new Comparator<Term>() {
+            @Override
+            public int compare(Term t1, Term t2) {
+                return t2.getExponent() - t1.getExponent();
             }
-        } else if (term.contains("^") && term.contains("x")) {
-            String coefficientTerm = term.substring(0, term.indexOf("x")); 
-            String exponentTerm = term.substring(term.indexOf("^") + 1); //!
-            this.exponent = Integer.parseInt(exponentTerm); //!
-            if(coefficientTerm.equals("+")) { //!
-                this.coefficient = 1;
-            } else if(coefficientTerm.equals("-")) { //!
-                this.coefficient = -1;
-            } else {
-                this.coefficient = Integer.parseInt(coefficientTerm); 
+        });
+    }
+    /**
+     * Adds another polynomial to this polynomial.
+     * @param other The polynomial to add to this polynomial.
+     * @return The resulting polynomial after addition.
+     */
+    public Polynomial add(Polynomial otherPolyObj) {
+        LinkedList<Term> poly = otherPolyObj.polynomialList;
+        this.polynomialList.addAll(poly);
+        this.addLikeTerms();
+    }
+
+    // how many terms are there?
+    public int getNumTerms() {
+        return this.polynomialList.size();
+    }
+
+    //get term
+    public Term getTerm(int index) {
+        return this.polynomialList.get(index);
+    }
+
+
+    
+    // clear all terms
+    public void clear() {
+        return this.polynomialList.clear(); 
+    }
+    
+    @Override
+    public String toString() {
+        String output = "";
+        if (this.polynomialList.isEmpty()) {
+            output = "0";
+        } else {
+            ListIterator<Term> iter = this.polynomialList.listIterator();
+            Term curr = iter.next();
+            if (curr.getCoefficient() > 0) {
+                output += curr.toString().substring(1);
+            }else {
+                output += curr.toString();
             }
+            while(iter.hasNext()) {
+                curr = iter.next();
+                output += iter.next().toString();
+            }
+        // if (terms.isEmpty()) {
+        //     return "0"; // handle case when polynomial is zero
+        // }
+        // StringBuilder sb = new StringBuilder();
+        // boolean isFirstTerm = true; //!
+        // for (Term t : terms) {
+        //     if (t.getCoefficient() == 0) {
+        //         continue; // Skip terms with a coefficient of 0
+        //     }
+        //     if (!isFirstTerm) { //!
+        //         if (t.getCoefficient() > 0) {
+        //             sb.append(" + ");
+        //         } else {
+        //             sb.append(" - ");
+        //             sb.append(Math.abs(t.getCoefficient()));
+        //         }
+        //     } else {
+        //         if (t.getCoefficient() < 0) {
+        //             sb.append("-");
+        //         }
+        //         sb.append(Math.abs(t.getCoefficient()));
+        //         isFirstTerm = false; //!
+        //     }
+        //     if (t.getExponent() != 0) {
+        //         sb.append("x");
+        //         if (t.getExponent() != 1) {
+        //             sb.append("^").append(t.getExponent());
+        //         }
+        //     }
         }
-    } 
-
-    // dupe constructor
-    public Term dupe() {
-        return new Term(this.coefficient, this.exponent);
-    }
-
-
-    
-    //setters
-    public void setCoefficient(int coefficient) {
-        this.coefficient = coefficient;
-    }
-    
-    public void setExponent(int exponent) {
-        this.exponent = exponent;;
-    }
-    
-    public void setTerm(int coefficient, int exponent) {
-        this.setCoefficient(coefficient);
-        this.setExponent(exponent);
-    }
-    
-    public void setAll(int coefficient, int exponent) {
-        this.setCoefficient(coefficient);
-        this.setExponent(exponent);
-    }
-    
-    //getters
-    public int getCoefficient() {
-        return this.coefficient;
-    }
-
-    public int getExponent() {
-        return this.exponent;
-    }
-
-
-    //toString
-
-    // from video:
-    // public String toString() {
-    //     String term = "";
-    //     if(this.coefficient == 0) {
-    //         return "";
-    //     } else if(this.exponent == 0 && this.coefficient > 0) {
-    //         return "" + this.coefficient;
-    //     } else if (this.exponent == 0 && this.coefficient < 0) {
-    //         return "" + this.coefficient;
-    //     } else if(this.exponent == 1 && this.coefficient == 1) {
-    //         return "+x";
-    //     } else if (this.exponent == 1 && this.coefficient == -1) {
-    //         return "-x";
-    //     } else if (this.exponent == 1 && this.coefficient > 1) {
-    //         return "+" + this.coefficient + "x";
-    //      } else if (this.exponent == 1 && this.coefficient < 1) {
-    //         return "" + this.coefficient + "x";
-    //     } else if (this.coefficient == 1) {
-    //         return "+x^" + this.exponent;
-    //     } else if (this.coefficient == -1) {
-    //         return "-x^" + this.exponent;
-    //     } else if (this.coefficient > 1) {
-    //         return "+" + "x^" + this.exponent;
-    //     } else if (this.coefficient < 1) {
-    //         return "" + "x^" + this.exponent;
-    //     }
-    //     return term;
-    // }
-
-// refactor:
-public String toString() {
-    if(this.coefficient == 0) {
-        return "0"; 
-    } else if(this.exponent == 0) {
-        return "" + this.coefficient; 
-    } else if(this.exponent == 1) {
-        if(this.coefficient == 1) {
-            return "x"; 
-        } else if(this.coefficient == -1) {
-            return "-x"; 
-        } else {
-            return this.coefficient + "x"; 
-        }
-    } else {
-        if(this.coefficient == 1) {
-            return "x^" + this.exponent; 
-        } else if(this.coefficient == -1) {
-            return "-x^" + this.exponent; 
-        } else {
-            return this.coefficient + "x^" + this.exponent; 
-        }
+        return output;
+        // return sb.toString();
     }
 }
 
-
-    //compareTo
-    public int compareTo(Term term) {
-        return this.exponent - term.exponent;
-    }
-
-    //equals
-    public boolean equals(Object obj) {
-        if(obj instanceof Term) {
-            Term other = (Term) obj;
-            return this.coefficient == other.coefficient && this.exponent == other.exponent;
-        }
-        return false;
-    }
-
-    //hashCode
-    public int hashCode() {
-        return coefficient + exponent;
-    }
-}
-//!
+// /**
+//  * Multiplies another polynomial with this polynomial.
+//  * @param other The polynomial to multiply with this polynomial.
+//  * @return The resulting polynomial after multiplication.
+//  */
+// public Polynomial multiply(Polynomial other) {
+//     Polynomial result = new Polynomial();
+//     for (Term t1 : this.terms) {
+//         for (Term t2 : other.terms) {
+//             int newCoefficient = t1.getCoefficient() * t2.getCoefficient();
+//             int newExponent = t1.getExponent() + t2.getExponent();
+//             result.addTerm(new Term(newCoefficient, newExponent));
+//         }
+//     }
+//     return result;
+// }
